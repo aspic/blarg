61c7851cd77fad860c865e7957e18195578d0d8c
Kjetil Mehl <aspic@mehl.no>
2012-10-30 00:38:49 +0100

    Splitting some logic. A sub module for handling custom text anchors.

--- lib/Blarg.pm
+++ lib/Blarg.pm
@@ -10,11 +10,17 @@ use Text::Markdown 'markdown';
 use Data::Dumper;
 use File::Slurp;
 
+# Custom modules
+use Blarg::Anchors;
+
 my $config;
+my $anchors = new Blarg::Anchors;
+my $blarg;
+my $self;
 
 sub new {
 	my $class = shift;
-	my $self = {};
+	$self = {};
 	bless $self, $class;
 	return $self;
 }
@@ -31,7 +37,7 @@ sub config {
 }
 
 sub strip_meta {
-	my ($self, $post) = @_;
+	my ($post) = @_;
 
 	my @lines = split(/\n/, $post->{content});
 
@@ -72,46 +78,10 @@ sub strip_meta {
 }
 
 
-sub inject_actions {
-	my ($self, $post) = @_;
-
-	my @lines = split(/\n/, $post->{content});
-	# Altered text
-	my @content = ();
-
-	for my $line (@lines) {
-		if( $line =~ m/^{/) {
-			# Custom hooks directly into md-file
-
-			# Extract options
-			my @cmd_options = split(/ /, substr ($line, 3, length($line) - 6));
-			my $command = shift @cmd_options;
-			my $options = {
-				cmd => $command,
-				file_name => $post->{file_name}
-			};
-
-			# Append arguments
-			for my $arg (@cmd_options) {
-				my ($key, $val) = split(/=/, $arg);
-				$options->{$key} = $val;
-			}
-			# Add result
-			push @content, $self->use_command($options, $post);
-		} else {
-			push @content, $line;
-		}
-	}
-
-	# Create references
-	$post->{content} = join("\n", @content);
-
-	return $post;
-}
 
 # Sequentially processes the raw markdown-file.
 sub process_file {
-	my ($self, $file_name) = @_;
+	my ($file_name) = @_;
 
 	my $posts = config('DIR_POSTS');
 	my $temp = read_file("$posts/$file_name");
@@ -123,10 +93,10 @@ sub process_file {
 	};
 
 	# Get meta data
-	$post = $self->strip_meta($post);
+	$post = strip_meta($post);
 
 	# Inject actions
-	$post = $self->inject_actions($post);
+	$post = $anchors->inject_anchors($self, $post);
 
 	return $post;
 }
@@ -138,7 +108,7 @@ sub process_file {
 sub create_file {
 	my ($self, $file_name) = @_;
 
-	my $post = $self->process_file($file_name);
+	my $post = process_file($file_name);
 
 	my $meta = $post->{meta};
 	my $file = $post->{file};
@@ -190,51 +160,9 @@ sub dir_layout {
 	return $root;
 }
 
-# We expect a hash, and should return text
-sub use_command {
-	my ($self, $cmd, $post) = @_;
-
-	my @result = ();
-	my $type = $cmd->{cmd};
-
-	# List N posts
-	if($type =~ m/posts/) {
-		for my $post ( @{ $self->get_posts() }) {
-			my $title = $post->{meta}->{title};
-			# Assign title
-			unless(defined($title)) {
-				$title = $post->{file_name};
-			}
-			# Create list
-			push @result, "-    [$title]($post->{file}->{path})\n";
-		}
-	}
-	# Single post
-	elsif($type =~ m/post/) {
-		my $last = $self->get_posts(1)->[0];
-		my $content = $last->{content};
-		push @result, $content;
-	}
-	# Git log
-	elsif($type =~ m/git/) {
-		my $log = git_log($cmd);
-		push @result, $log;
-	}
-	# Date
-	elsif($type =~ m/date/) {
-		my $file = $post->{file};
-		push @result, "    $file->{year}-$file->{month}-$file->{day}";
-	}
-
-	my $result = join("\n", @result);
-
-	return $result;
-
-}
-
 # Ghetto method for returning all posts.
 sub get_posts {
-	my ($self, $limit) = @_;
+	my ($limit) = @_;
 
 	unless(defined($limit)) {
 		$limit = 10;
@@ -246,7 +174,7 @@ sub get_posts {
 	opendir(DIR, config('DIR_POSTS')) or die $!;
 	while (my $file = readdir(DIR)) {
 		if($file =~ m/^[0-9]/) {
-			push @posts, $self->process_file($file);
+			push @posts, process_file($file);
 			$count++;
 			if($count >= $limit) {
 				last;
@@ -258,42 +186,6 @@ sub get_posts {
 	return \@posts;
 }
 
-# Returns the git log for the specified file
-sub git_log {
-	my ($cmd) = @_;
-
-	my $title = $cmd->{title};
-	my $path = $cmd->{path};
-	my $limit = $cmd->{limit};
-	my $style = $cmd->{style};
-
-	unless(defined($limit)) {
-		$limit = 1;
-	}
-	unless(defined($style)) {
-		$style = "    ";
-	}
-
-	my @result = ();
-	# Paths
-	unless(defined($path)) {
-		# We assume posts
-		$path = config('DIR_POSTS')."/".$cmd->{file_name};
-		@result = `git log -$limit --pretty=format:'%h - %s (%ci)' --abbrev-commit $path`;
-	} else {
-		# Custom directory (for projects etc)
-		my $git_dir = "$path/.git";
-		# Only check root
-		$path = ".";
-		@result = `git --git-dir=$git_dir log -$limit --pretty=format:'%h - %s (%ci)' --abbrev-commit $path`;
-	}
-
-	for my $item (@result) {
-		$item = $style.$item;
-	}
-
-	return join("", @result);
-}
 
 # Convenient (but ugly) routine for getting path info
 sub file_path {
---
+++ lib/Blarg/Anchors.pm
@@ -0,0 +1,141 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+package Blarg::Anchors;
+
+use Data::Dumper;
+
+my $self;
+my $blarg;
+
+sub new {
+	my ($class, $blarg_ref) = @_;
+	my $self = {};
+	bless $self, $class;
+
+	return $self;
+}
+
+#
+# Parses and injects anchors into the post
+sub inject_anchors {
+	my ($self, $blarg, $post) = @_;
+
+	my @lines = split(/\n/, $post->{content});
+	# Altered text
+	my @content = ();
+
+	for my $line (@lines) {
+
+		if($line =~ s/(.*\[% )(.*)( %\].*)/$1$3/) {
+			# Custom hooks directly into md-file
+			my $anchor = $2;
+
+			# Extract options
+			my @cmd_options = split(/ /, $anchor);
+
+			my $command = shift @cmd_options;
+			my $options = {
+				cmd => $command,
+				file_name => $post->{file_name},
+			};
+
+			# Append arguments
+			for my $arg (@cmd_options) {
+				my ($key, $val) = split(/=/, $arg);
+				$options->{$key} = $val;
+			}
+			# Add result
+			push @content, use_anchor($options, $post);
+		} else {
+			# Nothing to see here, move along
+			push @content, $line;
+		}
+	}
+	# Create references
+	$post->{content} = join("\n", @content);
+
+	return $post;
+}
+
+# Simple and naive routine for handling anchors
+sub use_anchor {
+	my ($cmd, $post) = @_;
+
+	my @result = ();
+	my $type = $cmd->{cmd};
+
+	# List N posts
+	if($type =~ m/posts/) {
+		for my $post ( @{ Blarg::get_posts() }) {
+			my $title = $post->{meta}->{title};
+			# Assign title
+			unless(defined($title)) {
+				$title = $post->{file_name};
+			}
+			# Create list
+			push @result, "-    [$title]($post->{file}->{path})\n";
+		}
+	}
+	# Single post
+	elsif($type =~ m/post/) {
+		my $last = $self->get_posts(1)->[0];
+		my $content = $last->{content};
+		push @result, $content;
+	}
+	# Git log
+	elsif($type =~ m/git/) {
+		my $log = git_log($cmd);
+		push @result, $log;
+	}
+	# Date
+	elsif($type =~ m/date/) {
+		my $file = $post->{file};
+		push @result, "    $file->{year}-$file->{month}-$file->{day}";
+	}
+
+	my $result = join("\n", @result);
+
+	return $result;
+
+}
+
+# Returns the git log for the specified file
+sub git_log {
+	my ($cmd) = @_;
+
+	my $title = $cmd->{title};
+	my $path = $cmd->{path};
+	my $limit = $cmd->{limit};
+	my $style = $cmd->{style};
+
+	unless(defined($limit)) {
+		$limit = 1;
+	}
+	unless(defined($style)) {
+		$style = "    ";
+	}
+
+	my @result = ();
+	# Paths
+	unless(defined($path)) {
+		# We assume posts
+		$path = Blarg::config('DIR_POSTS')."/".$cmd->{file_name};
+		@result = `git log -$limit --pretty=format:'%h - %s (%ci)' --abbrev-commit $path`;
+	} else {
+		# Custom directory (for projects etc)
+		my $git_dir = "$path/.git";
+		# Only check root
+		$path = ".";
+		@result = `git --git-dir=$git_dir log -$limit --pretty=format:'%h - %s (%ci)' --abbrev-commit $path`;
+	}
+
+	for my $item (@result) {
+		$item = $style.$item;
+	}
+
+	return join("", @result);
+}
+
+1;
